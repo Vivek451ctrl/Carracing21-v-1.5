5e80bd4c6898ee0469420bdfb70f22769e040af2
vivek <priyagole@gmail.com>
2020-11-27 15:40:30 +0530

    real racing

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1", car1i);
     car2 = createSprite(300,200);
+    car2.addImage("car2", car2i);
     car3 = createSprite(500,200);
+    car3.addImage("car3", car3i);
     car4 = createSprite(700,200);
+    car4.addImage("car4", car4i);
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background(ground);
+      image(track, 0, -displayHeight*4, displayWidth, displayHeight*5);
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
--- sketch.js
+++ sketch.js
@@ -9,7 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1i, car2i, car3i, car4i, track;
+var ground;
 
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1i = loadImage("images/car1.png");
+  car2i = loadImage("images/car2.png");
+  car3i = loadImage("images/car3.png");
+  car4i = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
